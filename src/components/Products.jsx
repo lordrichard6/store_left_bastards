e22b15dfc43abafa653c4b993edb334531bf36d7
lordrichard6 <paulolopesreizinho@gmail.com>
2021-04-26 22:02:22 +0200

    fetch the products to api

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,69 +2,14 @@ import React from "react";
 import { Grid } from "@material-ui/core";
 
 import Product from "./Product";
+import useStyles from "../styles/Products";
 
-const products = [
-  {
-    id: 1,
-    name: "Black Hoodie",
-    description: "Black hoodie with logo and name.",
-    price: "50CHF",
-    image: '../img/logo_noTitle_gold.png',
-  },
-  {
-    id: 2,
-    name: "White Hoodie",
-    description: "White hoodie with logo and name.",
-    price: "50CHF",
-    image: '../img/logo_noTitle_gold.png',
-  },
-  {
-    id: 3,
-    name: "Black Hoodie",
-    description: "Black hoodie with logo and name.",
-    price: "50CHF",
-    image: '../img/logo_noTitle_gold.png',
-  },
-  {
-    id: 4,
-    name: "White Hoodie",
-    description: "White hoodie with logo and name.",
-    price: "50CHF",
-    image: '../img/logo_noTitle_gold.png',
-  },
-  {
-    id: 5,
-    name: "Black Hoodie",
-    description: "Black hoodie with logo and name.",
-    price: "50CHF",
-    image: '../img/logo_noTitle_gold.png',
-  },
-  {
-    id: 6,
-    name: "White Hoodie",
-    description: "White hoodie with logo and name.",
-    price: "50CHF",
-    image: '../img/logo_noTitle_gold.png',
-  },
-  {
-    id: 7,
-    name: "Black Hoodie",
-    description: "Black hoodie with logo and name.",
-    price: "50CHF",
-    image: '../img/logo_noTitle_gold.png',
-  },
-  {
-    id: 8,
-    name: "White Hoodie",
-    description: "White hoodie with logo and name.",
-    price: "50CHF",
-    image: '../img/logo_noTitle_gold.png',
-  },
-];
+const Products = ({ products }) => {
+  const classes = useStyles();
 
-const Products = () => {
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
       <Grid container justify="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
